c3a179d7f37442c3f3378475474e6caf2acbee06
github username <github email address>
2019-07-04 12:44:54 -0400

    Done.

---
+++ artist.rb
@@ -0,0 +1,20 @@
+class Artist < ActiveRecord::Base
+
+    # @@all = []
+
+    # attr_accessor :genre, :age, :hometown
+    # attr_reader :name
+
+    # def initialize(name)
+    #     @name = name
+    #     @genre = genre
+    #     @age = age
+    #     @hometown = hometown
+
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+end
